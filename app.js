5f20061c2808f0d792f314a3e28a1eafcc021d52
Saqib <saqib29111@gmail.com>
2021-01-13 17:17:45 +0600

    complete cart page

--- app.js
+++ app.js
@@ -12,15 +12,21 @@ const PORT = process.env.PORT || 3000;
 // Assets
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
 
 // set Template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+});
 
 
 
