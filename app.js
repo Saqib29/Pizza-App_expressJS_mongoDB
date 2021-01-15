04813b336d030748893afb0f6e3fdba43835800b
Saqib <saqib29111@gmail.com>
2021-01-16 01:48:24 +0600

    removerd router to routes/web.js under a function

--- app.js
+++ app.js
@@ -18,25 +18,7 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
-
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/cart', (req, res) => {
-    res.render('customers/cart');
-});
-
-// Login 
-app.get('/login', (req, res) => {
-    res.render('auth/login');
-});
-
-// Register
-app.get('/register', (req, res) => {
-    res.render('auth/registration');
-});
+require('./routes/web')(app);
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
---
+++ routes/web.js
@@ -0,0 +1,26 @@
+
+
+function initRoutes(app){
+    app.get('/', (req, res) => {
+        res.render('home');
+    });
+
+    
+    app.get('/cart', (req, res) => {
+        res.render('customers/cart');
+    });
+
+    // Login 
+    app.get('/login', (req, res) => {
+        res.render('auth/login');
+    });
+
+    // Register
+    app.get('/register', (req, res) => {
+        res.render('auth/registration');
+    });
+
+}
+
+
+module.exports = initRoutes;
