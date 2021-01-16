e7b755f7784054ee7f1008296254953cdfa8e629
Saqib <saqib29111@gmail.com>
2021-01-16 16:43:38 +0600

    add factory function in auth & home comtroller for login, register & home

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,4 +1,12 @@
 
 function homeController(){
     
+    return {
+        index(req, res) {
+            res.render('home');
+        }
+    }
 }
+
+
+module.exports = homeController;
--- routes/web.js
+++ routes/web.js
@@ -1,23 +1,19 @@
+const homeController = require('../app/http/controllers/homeController');
+const authController = require('../app/http/controllers/authController');
 
 function initRoutes(app){
-    app.get('/', (req, res) => {
-        res.render('home');
-    });
-
+    app.get('/', homeController().index);
+    
     
     app.get('/cart', (req, res) => {
         res.render('customers/cart');
     });
 
     // Login 
-    app.get('/login', (req, res) => {
-        res.render('auth/login');
-    });
+    app.get('/login', authController().login);
 
     // Register
-    app.get('/register', (req, res) => {
-        res.render('auth/registration');
-    });
+    app.get('/register', authController().register);
 
 }
 
