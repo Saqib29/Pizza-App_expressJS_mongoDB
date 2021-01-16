853877370529af9de54c86cbfeb66fcd10f58727
Saqib <saqib29111@gmail.com>
2021-01-16 19:52:19 +0600

    connect to mongoDB

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const app               = express();
 const path              = require('path');
 const ejs               = require('ejs');
 const expressLayout     = require('express-ejs-layouts');
-
+const mongoose          = require('mongoose');
 
 
 const PORT = process.env.PORT || 3000;
+
+// Database connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected');
+}).catch(err => {
+    console.log('connection failed');
+});
+
 
 // Assets
 app.use(express.static('public'));
