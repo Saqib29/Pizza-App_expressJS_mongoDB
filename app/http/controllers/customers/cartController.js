4b3a0ff178b931239af6cb0afba012df1a7c5150
Saqib <saqib29111@gmail.com>
2021-01-17 14:30:20 +0600

    add axios to use ajax adding to cart

--- app/http/controllers/customers/cartController.js
+++ app/http/controllers/customers/cartController.js
@@ -3,6 +3,24 @@ function cartController(){
     return {
         index(req, res) {
             res.render('customers/cart');
+        },
+
+        update(req, res) {
+
+            // creating cart for the first time & adding basic object stracture
+            if(!req.session.cart){
+                req.session.cart = {
+                    items: {},
+                    totalQty: 0,
+                    totalPrice: 0
+                };
+            }
+
+            let cart = req.session.cart;
+
+            
+
+             res.json({ data: "all ok"});
         }
     }
 }
